8e21c86a4929e8efe37fd3a2c210d3124e491ac2
Hyejin Hong <37792092+hyejinhong@users.noreply.github.com>
2025-04-07 08:48:56 +0900

    Logging aspect

---
+++ kong-dic/src/main/java/com/kong/kong_dic/global/aspect/ControllerLoggingAspect.java
@@ -0,0 +1,42 @@
+package com.kong.kong_dic.global.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@Aspect
+@Component
+@Slf4j
+public class ControllerLoggingAspect {
+    @Around("execution(* com.kong.kong_dic..*Controller.*(..))")
+    public Object logControllerExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        String methodName = joinPoint.getSignature().getName();
+
+        // 파라미터 로깅
+        String params = Arrays.stream(joinPoint.getArgs())
+                .map(arg -> arg != null ? arg.toString() : "null")
+                .collect(Collectors.joining(", "));
+
+        log.info(">>> [Request Start] {}.{}({})", className, methodName, params);
+
+        long start = System.currentTimeMillis();
+
+        try {
+            Object result = joinPoint.proceed();
+            long duration = System.currentTimeMillis() - start;
+
+            log.info("<<< [Request Completed] {}.{} (Elapsed Time: {} ms)", className, methodName, duration);
+            return result;
+
+        } catch (Throwable throwable) {
+            log.error("!!! [Exception] {}.{} - {}", className, methodName, throwable.getMessage());
+            throw throwable;
+        }
+    }
+}
