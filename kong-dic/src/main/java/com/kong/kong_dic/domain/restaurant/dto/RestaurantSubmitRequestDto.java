cbc4bf105ec776ca58476eb7161bc069ecbdcdf3
Hyejin Hong <37792092+hyejinhong@users.noreply.github.com>
2025-05-28 08:50:29 +0900

    제출목록 조회시 ID 필드 추가

--- kong-dic/src/main/java/com/kong/kong_dic/domain/restaurant/dto/RestaurantSubmitRequestDto.java
+++ kong-dic/src/main/java/com/kong/kong_dic/domain/restaurant/dto/RestaurantSubmitRequestDto.java
@@ -14,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 public class RestaurantSubmitRequestDto {
 
+    private Long id;
     private String name;
     private String address;
     private List<BeanType> beanTypes;
--- kong-dic/src/main/java/com/kong/kong_dic/global/config/CorsConfig.java
+++ kong-dic/src/main/java/com/kong/kong_dic/global/config/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(frontUrl));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(List.of("*"));
 
