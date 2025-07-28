3a595b4a592c9e0d7f89fba9b252ae1ca812e690
Hyejin Hong <37792092+hyejinhong@users.noreply.github.com>
2025-07-29 08:48:09 +0900

    feat: 어드민 서비스 백엔드 프로젝트 초기 설정

---
+++ kong-dic-admin/src/main/java/com/kong/kong_dic_admin/KongGuksuAdminServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kong.kong_dic_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KongGuksuAdminServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KongGuksuAdminServiceApplication.class, args);
+	}
+
+}
---
+++ kong-dic-admin/src/test/java/com/kong/kong_dic_admin/KongGuksuAdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kong.kong_dic_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KongGuksuAdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
