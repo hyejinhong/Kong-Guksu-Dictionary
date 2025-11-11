9f40b1197b3486cca55139a8a8bdfa35c62d01a7
Hyejin Hong <37792092+hyejinhong@users.noreply.github.com>
2025-11-12 08:45:33 +0900

    refactor: BaseException 메시지 오버라이드 로직 추가
    - BaseException 클래스에 커스텀 메시지 필드를 추가하고, 생성자를 통해 메시지를 주입받아 예외 메시지를 유연하게 재정의할 수 있도록 로직을 개선

--- kong-dic-common/src/main/java/com/kong/kong_dic/common/exception/BaseException.java
+++ kong-dic-common/src/main/java/com/kong/kong_dic/common/exception/BaseException.java
@@ -5,10 +5,18 @@ import lombok.Getter;
 @Getter
 public class BaseException extends RuntimeException {
     private final BaseExceptionType exceptionType;
+    private String customMessage;
 
     public BaseException(BaseExceptionType exceptionType) {
         super(exceptionType.getMessage());
         this.exceptionType = exceptionType;
+    }
+
+    // 오버라이트할 메시지 있는 경우
+    public BaseException(BaseExceptionType exceptionType, String message) {
+        super(exceptionType.getMessage());
+        this.exceptionType = exceptionType;
+        this.customMessage = message;
     }
 
     public int getCode() {
@@ -17,5 +25,10 @@ public class BaseException extends RuntimeException {
 
     public int getHttpStatusCode() {
         return exceptionType.getHttpStatusCode();
+    }
+
+    @Override
+    public String getMessage() {
+        return customMessage != null ? customMessage : exceptionType.getMessage();
     }
 }
