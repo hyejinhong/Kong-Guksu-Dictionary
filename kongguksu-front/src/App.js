63bdfd69e37360e362e8883baec051a1d0829a52
Hyejin Hong <37792092+hyejinhong@users.noreply.github.com>
2025-11-13 09:23:26 +0900

    feat: 사용자 마이페이지 프로필 수정 기능 구현
    - 마이페이지(/mypage)에 접속 시 사용자 정보를 조회하고 수정할 수 있는 MyPageProfile 컴포넌트를 구현
    - 닉네임 수정 및 비밀번호 변경 로직(현재 비밀번호 확인 포함)을 프론트엔드에 추가하고, 백엔드 API (GET/PATCH /users/me)와 연동
    - BaseLayout 헤더에 '내 정보' 링크를 추가

--- kongguksu-front/src/App.js
+++ kongguksu-front/src/App.js
@@ -9,25 +9,28 @@ import AdminRestaurantSubmissions from "./pages/AdminRestaurantSubmissions";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import MyDictionary from "./pages/MyDictionary";
+import MyPageProfile from "./pages/MyPageProfile";
+
 import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/" element={<MainLayout />}> {/* MainLayout으로 감싸기 */}
-          <Route index element={<HomePage />} /> {/* index 라우트로 홈페이지 설정 */}
-          <Route path="/restaurant/:id" element={<RestaurantDetailPage />} />
-          <Route path="/visited-restaurants" element={<MyDictionary/>}/>
-          <Route path="/submit-restaurant" element={<RestaurantSubmissionForm />} />
-          <Route path="/admin/restaurant-submissions" element={<AdminRestaurantSubmissions />} />
-        </Route>
-      </Routes>
-    </Router>
-    <Toaster position="top-right" reverseOrder={false} />
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/" element={<MainLayout />}> {/* MainLayout으로 감싸기 */}
+            <Route index element={<HomePage />} /> {/* index 라우트로 홈페이지 설정 */}
+            <Route path="/restaurant/:id" element={<RestaurantDetailPage />} />
+            <Route path="/visited-restaurants" element={<MyDictionary />} />
+            <Route path="/submit-restaurant" element={<RestaurantSubmissionForm />} />
+            <Route path="/admin/restaurant-submissions" element={<AdminRestaurantSubmissions />} />
+            <Route path="/mypage" element={<MyPageProfile />} />
+          </Route>
+        </Routes>
+      </Router>
+      <Toaster position="top-right" reverseOrder={false} />
     </>
   );
 }
