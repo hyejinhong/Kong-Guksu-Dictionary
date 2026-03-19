5d9eb6c50a1769d491b2d6f80373a8a156186094
Hyejin Hong <37792092+hyejinhong@users.noreply.github.com>
2026-03-20 08:49:35 +0900

    refactor: 환경변수명 변경

--- kongguksu-front/vercel.ts
+++ kongguksu-front/vercel.ts
@@ -2,6 +2,6 @@ import { routes, deploymentEnv, type VercelConfig } from '@vercel/config/v1';
 
 export const config: VercelConfig = {
   rewrites: [
-    routes.rewrite('/api/:path*', `${deploymentEnv('REACT_APP_API_BASE_URL')}/:path*`)
+    routes.rewrite('/api/:path*', `${deploymentEnv('GCP_MAIN_URL')}/:path*`)
   ],
 };
