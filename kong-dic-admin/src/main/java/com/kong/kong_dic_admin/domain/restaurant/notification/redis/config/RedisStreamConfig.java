82d634be2ff831e5a4e696ccd673e9a2d1459f1f
Hyejin Hong <hhj732@naver.com>
2026-03-19 22:52:23 +0900

    fix: 어드민 서버 Redis 연결 오류(Connection refused) 수정
    - LettuceConnectionFactory 기본 생성자 사용 시 기본값(localhost)으로 연결되는 문제 해결
    - application.yml의 host, port 환경변수를 명시적으로 주입하여 Redis 컨테이너와 정상 연결되도록 수정

--- kong-dic-admin/src/main/java/com/kong/kong_dic_admin/domain/restaurant/notification/redis/config/RedisStreamConfig.java
+++ kong-dic-admin/src/main/java/com/kong/kong_dic_admin/domain/restaurant/notification/redis/config/RedisStreamConfig.java
@@ -1,8 +1,10 @@
 package com.kong.kong_dic_admin.domain.restaurant.notification.redis.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -10,9 +12,18 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 @Configuration
 public class RedisStreamConfig {
 
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisHost);
+        redisStandaloneConfiguration.setPort(redisPort);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
