7e65c50dc59e811b56e254b5cc3fcb902e3ca919
Hyejin Hong <37792092+hyejinhong@users.noreply.github.com>
2026-07-01 07:23:34 +0900

    fix: JPA N+1 성능 최적화 (EntityGraph 및 batch_fetch_size 설정)

--- kong-dic/src/main/java/com/kong/kong_dic/domain/restaurant/repository/RestaurantCommentRepository.java
+++ kong-dic/src/main/java/com/kong/kong_dic/domain/restaurant/repository/RestaurantCommentRepository.java
@@ -4,11 +4,13 @@ import com.kong.kong_dic.domain.restaurant.entity.RestaurantComment;
 import com.kong.kong_dic.domain.restaurant.entity.Restaurant;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface RestaurantCommentRepository extends JpaRepository<RestaurantComment, Long> {
+    @EntityGraph(attributePaths = {"author"})
     Page<RestaurantComment> findByRestaurantIdOrderByCreatedAtDesc(Long restaurantId, Pageable pageable);
 
     @Query(value = "SELECT c FROM RestaurantComment c " +
